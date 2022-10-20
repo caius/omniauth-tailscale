881059d54ea9972387a79db09208a90d144b5ff9
Caius Durling <dev@caius.name>
2022-10-20 09:48:58 +0100

    Debugging work in progress, cheating at auth

--- lib/omniauth/strategies/tailscale.rb
+++ lib/omniauth/strategies/tailscale.rb
@@ -7,9 +7,31 @@ module OmniAuth
       include OmniAuth::Strategy
 
       def request_phase
-        raise :request_phase
+        # TODO: hit tailscale's localapi to authenticate ourselves?
+
+        p request.env["REMOTE_ADDR"]
+
+        p callback_url
+
+        # FIXME: include tailscale params somehow - query string?
+        redirect "#{callback_url}?login_name=caius%40caius.name&display_name=Caius%20Durling"
       end
 
+      def auth_hash
+        binding.break
+        super
+      end
+
+      uid do
+        request.params["login_name"]
+      end
+
+      info do
+        {
+          display_name: request.params["display_name"],
+          profile_pic_url: request.params["profile_pic_url"]
+        }
+      end
     end
   end
 end
